ffce19fac6a59ea998bdd85d6abe11d2f120a4af
BoomStickk <6endgame6@gmail.com>
2022-06-19 02:00:52 +0300

    check valid imput debug

--- src/Hangman.java
+++ src/Hangman.java
@@ -8,13 +8,13 @@ import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner keyboard = new Scanner(System.in);
-        String word;
+        String city;
         System.out.print("Choose single or multiplayer(1or2): ");
-        Scanner name=new Scanner(System.in);
-        int players=name.nextInt();
-        String player1="";
-        String player="";
-        String player2="";
+        Scanner name = new Scanner(System.in);
+        int players = name.nextInt();
+        String player1 = "";
+        String player = "";
+        String player2 = "";
 
         while (true) {
             if (players == 2) {
@@ -22,9 +22,10 @@ public class Hangman {
                 player1 = name.next();
                 System.out.print("Enter player 2 name: ");
                 player2 = name.next();
-                player=player1;
+                player = player1;
                 break;
-            } else if (players == 1) {break;
+            } else if (players == 1) {
+                break;
             } else {
                 System.out.print("Invalid input, try again: ");
                 players = name.nextInt();
@@ -32,39 +33,38 @@ public class Hangman {
         }
 
 
-
         Scanner in = new Scanner(new File("C:\\Users\\Boom\\Documents\\java\\Hangman\\src\\cities.txt"));
 
-            List<String> words = new ArrayList<>();
+        List<String> words = new ArrayList<>();
 
-            //picking random word
-            while (in.hasNext()) {
-                words.add(in.nextLine());
-            }
-            Random rand = new Random();
-            word = words.get(rand.nextInt(words.size()));
+        //picking random word
+        while (in.hasNext()) {
+            words.add(in.nextLine());
+        }
+        Random rand = new Random();
+        city = words.get(rand.nextInt(words.size()));
 
 
         List<Character> playerGuess = new ArrayList<>();
-        printWordState(word, playerGuess);
+        printWordState(city, playerGuess);
         int wrongCount = 0;
         while (true) {
             printHangManState(wrongCount);
             if (wrongCount >= 6) {
                 System.out.println("You lose!");
-                System.out.println("The word was: "+word);
+                System.out.println("The city was: " + city);
                 break;
             }
-            if (!inputPlayerGuess(keyboard, word, playerGuess,player)) {           //hmm
+            if (!inputPlayerGuess(keyboard, city, playerGuess, player)) {           //hmm
                 wrongCount++;
-                player=player2;
-                player2=player1;
-                player1=player;
+                player = player2;
+                player2 = player1;
+                player1 = player;
 
             }
 
-            if (printWordState(word, playerGuess)) {
-                System.out.println(player+" "+"You win!");
+            if (printWordState(city, playerGuess)) {
+                System.out.println(player + " " + "You win!");
                 break;
             }
         }
@@ -117,13 +117,13 @@ public class Hangman {
         }
     }
 
-    private static boolean inputPlayerGuess(Scanner keyboard, String word, List<Character> playerGuess,String player) {
-        System.out.print(player+" please enter a letter: ");
+    private static boolean inputPlayerGuess(Scanner keyboard, String word, List<Character> playerGuess, String player) {
+        System.out.print(player + " please enter a letter: ");
         String letterGuess = keyboard.nextLine();
         //check for valid input
         while (letterGuess.length() != 1 || Character.isDigit(letterGuess.charAt(0))) {
             System.out.println("Invalid input, try again");
-            letterGuess = keyboard.next();
+            letterGuess = keyboard.nextLine();
         }
         playerGuess.add(letterGuess.charAt(0));
         return word.contains(letterGuess);
